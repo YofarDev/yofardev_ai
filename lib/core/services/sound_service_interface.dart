a358b61e6c2c2127dd255a92228b4bf7ef9b4541
Yoann Faraus <yofardev@gmail.com>
2026-03-01 23:46:24 +0100

    refactor: remove old sound_service_interface, replaced with SoundRepository

--- lib/core/services/sound_service_interface.dart
+++
@@ -1,5 +0,0 @@
-/// Abstract interface for SoundService to allow for dependency injection
-abstract class ISoundService {
-  /// Play a sound effect by name
-  Future<void> playSound(String soundName);
-}
